bca04361a512cfafcd88259c7d8102f8536ec85b
jugonan <jaf.mendizabal@gmail.com>
2020-05-21 16:48:12 +0200

    vm largo

---
+++ Heldu.Logic/ViewModels/ProductoCategoriaCondicionesVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Heldu.Entities.Models;
+
+namespace Heldu.Logic.ViewModels
+{
+    public class ProductoCategoriaCondicionesVM
+    {
+        public Producto Producto { get; set; }
+        public Categoria Categoria { get; set; }
+        public Condicion Condicion { get; set; }
+    }
+}
