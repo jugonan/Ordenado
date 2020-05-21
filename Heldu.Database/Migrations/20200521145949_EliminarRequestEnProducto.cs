6ea384ea0ef926648d37c7940897fbc620f50439
Galo García Pugliese <galo130@hotmail.com>
2020-05-21 17:01:43 +0200

    add-migration EliminarRequestEnProducto

---
+++ Heldu.Database/Migrations/20200521145949_EliminarRequestEnProducto.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DEFINITIVO.Migrations
+{
+    public partial class EliminarRequestEnProducto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Titulo",
+                table: "Producto",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PrecioFinal",
+                table: "Producto",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Producto",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Titulo",
+                table: "Producto",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PrecioFinal",
+                table: "Producto",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Producto",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
