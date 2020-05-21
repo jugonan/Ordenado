2fdf211d371cb4355d7651b08d08d13211df5422
Galo García Pugliese <galo130@hotmail.com>
2020-05-21 12:57:45 +0200

    add-migration CondicionProductos y update-database  OK

---
+++ Heldu.Database/Migrations/20200521105626_CondicionProductos.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DEFINITIVO.Migrations
+{
+    public partial class CondicionProductos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Condicion",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Reserva = table.Column<string>(nullable: true),
+                    Horario = table.Column<string>(nullable: true),
+                    Entrega = table.Column<string>(nullable: true),
+                    Recogida = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Condicion", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Condicion");
+        }
+    }
+}
