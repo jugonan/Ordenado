4a69094d4cfb840108e21100e67aaa44f0585202
jugonan <jaf.mendizabal@gmail.com>
2020-05-25 09:43:39 +0200

    Gondor

--- Heldu.Entities/Models/Condicion.cs
+++ Heldu.Entities/Models/Condicion.cs
@@ -7,17 +7,12 @@ namespace Heldu.Entities.Models
     public class Condicion
     {
         public int Id { get; set; }
-        [NotMapped]
         public List<string> Reserva { get; set; }
-        [NotMapped]
         public List<string> Horario { get; set; }
-        [NotMapped]
         public List<string> Entrega { get; set; }
-        [NotMapped]
         public List<string> Recogida { get; set; }
         public int ProductoId { get; set; }
         public Producto Producto { get; set; }
-        [NotMapped]
         public List<string> Otros { get; set; }
     }
 }
