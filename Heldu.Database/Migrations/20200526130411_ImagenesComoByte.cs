21fad8fde1b72386609ad9acc9ef8d12b8c3a4b4
Galo García Pugliese <galo130@hotmail.com>
2020-05-26 15:11:06 +0200

    Modificado el modelo de Producto. Las imagenes como byte y fuera los Data Annotations que no se utilizan

---
+++ Heldu.Database/Migrations/20200526130411_ImagenesComoByte.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DEFINITIVO.Migrations
+{
+    public partial class ImagenesComoByte : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "ImagenProducto3",
+                table: "Producto",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "ImagenProducto2",
+                table: "Producto",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "ImagenProducto",
+                table: "Producto",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImagenProducto3",
+                table: "Producto",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImagenProducto2",
+                table: "Producto",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImagenProducto",
+                table: "Producto",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: false,
+                oldClrType: typeof(byte[]));
+        }
+    }
+}
