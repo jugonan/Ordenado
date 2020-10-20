5b0dbea2549ebdc4f053550b6ce2d33ea9d6c719
Galo García Pugliese <galo130@gmail.com>
2020-10-20 17:18:09 +0200

    Intento de PWA, aun sin funcionar

--- DEFINITIVO/Startup.cs
+++ DEFINITIVO/Startup.cs
@@ -30,6 +30,7 @@ namespace DEFINITIVO
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddProgressiveWebApp();
             //StripeConfiguration.ApiKey = Configuration["Stripe:ApiKey"];
             services.AddMemoryCache();
             services.AddOutputCaching();
